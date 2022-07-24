241cc19b9a5b25eb7aa6433ba4ad62e884707552
skelmis <ethan@koldfusion.xyz>
2022-07-24 23:19:23 +1200

    feat: Add the ability to view paper outlines from all semesters

--- assignmentWebscraperDraft.py
+++ assignmentWebscraperDraft.py
@@ -5,7 +5,11 @@ import requests  # pip install requests
 from bs4 import BeautifulSoup, Tag  # pip install bs4
 
 paper_code = input("Whats your paper code? ")
-url = f"https://paperoutlines.waikato.ac.nz/outline/{paper_code.upper()}-22B%20(HAM)"
+sem = input("What semester? (A|B|H|C|G) ")
+if sem.upper() not in ("A", "B", "H", "C", "G"):
+    raise RuntimeError(f"{sem.upper()} is not a supported semester.")
+
+url = f"https://paperoutlines.waikato.ac.nz/outline/{paper_code.upper()}-22{sem.upper()}%20(HAM)"
 
 r = requests.get(url)
 if r.status_code != 200:
@@ -15,7 +19,9 @@ soup: BeautifulSoup = BeautifulSoup(r.content, "html.parser")
 
 table: Optional[Tag] = soup.find("div", id="module-Assessment-grid")
 if not table:
-    raise RuntimeError(f"'{paper_code}' does not appear to be a valid paper code.")
+    raise RuntimeError(
+        f"'{paper_code}' in {sem} semester does not appear to be a valid paper code."
+    )
 
 table_data: Tag = table.find("tbody")
 
