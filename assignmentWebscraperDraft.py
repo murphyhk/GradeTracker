cd569e01849b48081ac85eaf19139b3d8cdbeabb
Hannah <84253495+murphyhk@users.noreply.github.com>
2022-07-24 15:40:22 +1200

    Initial code for a webscraper to get assignment information from moodle. Initial code from skelmis

---
+++ assignmentWebscraperDraft.py
@@ -0,0 +1,35 @@
+from pprint import pprint
+from typing import NamedTuple, Optional, List
+
+import requests  # pip install requests
+from bs4 import BeautifulSoup, Tag  # pip install bs4
+
+paper_code = input("Whats your paper code? ")
+url = f"https://paperoutlines.waikato.ac.nz/outline/{paper_code.upper()}-22B%20(HAM)"
+
+r = requests.get(url)
+if r.status_code != 200:
+    raise RuntimeError(r.text)
+
+soup: BeautifulSoup = BeautifulSoup(r.content, "html.parser")
+
+table: Optional[Tag] = soup.find("div", id="module-Assessment-grid")
+if not table:
+    raise RuntimeError(f"'{paper_code}' does not appear to be a valid paper code.")
+
+table_data: Tag = table.find("tbody")
+
+
+class Entry(NamedTuple):
+    name: str
+    percentage: int
+
+
+assessments: List[Entry] = []
+for row in table_data.children:
+    cols: List[str] = [ele.text.strip() for ele in row]
+    name = cols[0].split(".")[1].lstrip("\xa0")
+    entry: Entry = Entry(name=name, percentage=int(cols[3]))
+    assessments.append(entry)
+
+pprint(assessments)
